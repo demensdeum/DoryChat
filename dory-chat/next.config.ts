9005a055a09391bb9d4329d37751cfdfe9784a4f
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2025-12-13 14:37:30 +0400

    prod config

--- dory-chat/next.config.ts
+++ dory-chat/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.NODE_ENV === 'production' 
+              ? '/dorychat-app' 
+              : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
